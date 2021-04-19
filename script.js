99a3d6b323a2273ddbde6076be878809661078e3
ilin.a@digitalbis.ru <ilin.a@digitalbis.ru>
2021-04-19 12:59:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+define(['jquery', 'lib/components/base/modal', 'lib/network/leads/api', './lib/intlTelInput/js/intlTelInput.js'], function ($, Modal) {
+
+    var CustomWidget = function () {
+        var self = this,
+            system = self.system(),
+ 
+        return this;
+
+    };
+
+    return CustomWidget;
+});
